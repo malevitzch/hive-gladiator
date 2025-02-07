a7d7ce3f5790592dd0123d5a23caed91f994dfa7
malevitzch <138793442+malevitzch@users.noreply.github.com>
2025-02-07 15:27:07 +0100

    Converted the project to a Maven project which should hopefully work now

--- hgladiator/src/application/Main.java
+++ hgladiator/src/application/Main.java
@@ -1,7 +1,7 @@
 package application;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Hive");
+	public static void main(String[] args) {
+        System.out.println("Welcome to the Hive, gladiator");
     }
 }
