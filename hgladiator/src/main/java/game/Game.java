447be0f4e0c9084485e180406ee2f4ef6ebe2e3a
malevitzch <138793442+malevitzch@users.noreply.github.com>
2025-02-08 12:19:01 +0100

    Added some more base functions for Game and GameState classes

--- hgladiator/src/main/java/game/Game.java
+++ hgladiator/src/main/java/game/Game.java
@@ -3,12 +3,17 @@ import player.Player;
 
 public class Game {
 	Player player;
+	GameState state;
 	public Game(Player p){
 		player = p;
+		state = new GameState();
 	}
 	public Game() {
 	}
 	public void play() { 
 		System.out.println("Welcome to the hive");
+	}
+	public Boolean nextTurn() {
+		return !state.isOver();
 	}
 }
--- hgladiator/src/main/java/game/GameState.java
+++ hgladiator/src/main/java/game/GameState.java
@@ -1,5 +1,22 @@
 package game;
 
-public class GameState {
+import player.move.Move;
 
+public class GameState {
+	private int score;
+	public Boolean isValid(Move m) {
+		//TODO: Validate the move legality
+		return true;
+	}
+	public Boolean makeMove(Move m) {
+		if(!isValid(m)) return false;
+		//TODO: Code goes here
+		return true;
+	}
+	public Boolean isOver() {
+		return false;
+	}
+	public int getScore() {
+		return score;
+	}
 }
