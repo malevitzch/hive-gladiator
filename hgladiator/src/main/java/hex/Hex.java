d4e6404e3f85b2b8b9d648dd95819c9c4e9cd719
malevitzch <138793442+malevitzch@users.noreply.github.com>
2025-02-10 19:12:44 +0100

    Created basic classes for Hexagons in a hexagonal board

---
+++ hgladiator/src/main/java/hex/Hex.java
@@ -0,0 +1,43 @@
+package hex;
+
+import java.util.ArrayList;
+
+public class Hex<V> {
+	// Data kept by the Hex
+	private V data;
+	
+	// Coordinates using the Cube coordinate system
+	private HexCoord coordinates;
+	
+	public Hex(int q, int r, int s, V data) {
+		coordinates = new HexCoord(q, r, s);
+	}
+	
+	public V getData() {
+		return data;
+	}
+	public int getQ() {
+		return coordinates.q;
+	}
+	public int getR() {
+		return coordinates.r;
+	}
+	public int getS() {
+		return coordinates.s;
+	}
+	public ArrayList<HexCoord> getNeighbourCoords() {
+		ArrayList<HexCoord> coordList = new ArrayList<HexCoord>();
+		int q = coordinates.q;
+		int r = coordinates.r;
+		int s = coordinates.s;
+		
+		coordList.add(new HexCoord(q, r-1, s+1));
+		coordList.add(new HexCoord(q, r+1, s-1));
+		coordList.add(new HexCoord(q-1, r, s+1));
+		coordList.add(new HexCoord(q+1, r, s-1));
+		coordList.add(new HexCoord(q-1, r+1, s));
+		coordList.add(new HexCoord(q+1, r-1, s));
+		
+		return coordList;
+	}
+}
---
+++ hgladiator/src/main/java/hex/HexCoord.java
@@ -0,0 +1,10 @@
+package hex;
+
+public class HexCoord {
+	public int q, r, s;
+	public HexCoord(int q, int r, int s) {
+		this.q = q;
+		this.r = r;
+		this.s = s;
+	}
+}
